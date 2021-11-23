d7e31ce17f0c2b903f4b2262ec171b0a9aaedc19
0xCarnyx <0xCarnyx@protonmail.com>
2021-11-23 22:46:28 +0100

    update NFT class

--- NFT.py
+++ NFT.py
@@ -1,13 +1,53 @@
+from abc import ABC, abstractmethod
+
 import requests
 
 
-class NFT:
+class NFT(ABC):
+    """Represents an NFT collection.
+
+    """
+    @abstractmethod
+    def get_stats(self) -> dict:
+        """Retrieves dictionary with statistics about the NFT collection. Used to retrieve detailed statistics.
+
+        :return: Statistics dictionary
+        """
+        pass
+
+    @abstractmethod
+    def get_floor_price(self) -> float:
+        """Retrieves the current floor price in ETH for the NFT collection.
+
+        :return: Floor price in ETH
+        """
+        pass
+
+    @abstractmethod
+    def get_volume(self, period: str) -> float:
+        """Retrieves the volume in ETH of the NFT collection during a given time period.
+
+        :param period: Time period
+        :return: Volume in ETH during the given time period
+        """
+        pass
+
+    @abstractmethod
+    def get_num_owners(self) -> float:
+        """Return the number of addresses which own at least one NFT of the NFT collection.
+
+        :return: Number of addresses which possess at least one NFT of the NFt collection
+        """
+        pass
+
+
+class OpenseaNFT(NFT):
     def __init__(self, asset: str):
         self.asset = asset
 
     def get_stats(self) -> dict:
         response = requests.get(f"https://api.opensea.io/api/v1/collection/{self.asset}")
-        return response.json().get("stats")
+        return response.json().get("collection").get("stats")
 
     def get_floor_price(self) -> float:
         stats = self.get_stats()
